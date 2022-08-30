0869e73c7599304744433fe3359f2aa4539d98c7
tony dugué <duguetony@gmail.com>
2022-08-30 02:14:43 +0200

    add fonts and twin-macro library for combinated Styled-components & tailwind

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const withTM = require('next-transpile-modules')(['gsap'])
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  reactStrictMode: false,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false }
+
+    return config;
+  }
 }
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next'
 import { METADATA } from "../constants";
 import Head from 'next/head'
-import tw from "tailwind-styled-components"
+
+import styled from "styled-components";
+import tw from "twin.macro";
 
 const Home: NextPage = () => {
   return (
@@ -16,13 +18,17 @@ const Home: NextPage = () => {
         <First>First</First>
         <Second>Second</Second>
       </Wrapper>
-
     </>
   )
 }
 
 export default Home
 
-const Wrapper = tw.div`flex flex-col h-screen bg-red-300`
-const First = tw.div`bg-red-500 flex-1`
-const Second = tw.div`flex-1`
+const Wrapper = styled.div`${tw`flex flex-col h-screen bg-red-300`}`
+const First = styled.div`${tw`bg-red-500 flex-1`}`
+
+const Second = styled.div`
+  color: yellow;
+  ${tw`flex-1`}
+`
+
