650964bf1ad1ab041b752edf27ddb9e066f3c50e
tony dugué <duguetony@gmail.com>
2022-08-30 23:44:54 +0200

    add Layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,25 @@
 import type { NextPage } from 'next'
-import { METADATA } from "../constants";
 import Head from 'next/head'
+import { METADATA } from "../constants";
+import Layout from '../components/Layout'
 
-import styled from "styled-components";
-import tw from "twin.macro";
+//import styled from "styled-components";
+//import tw from "twin.macro";
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>{METADATA.title}</title>
-        <meta name="description" content={METADATA.description} />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Wrapper>
-        <First>First</First>
-        <Second>Second</Second>
-      </Wrapper>
+      <Layout>
+        <h1>Test</h1>
+      </Layout>
     </>
   )
 }
 
 export default Home
 
-const Wrapper = styled.div`${tw`flex flex-col h-screen bg-red-300 lg:pl-12 lg:pr-12`}`
-const First = styled.div`${tw`bg-red-500 flex-1`}`
-
-const Second = styled.div`${tw`flex-1`}`
-
+//const Wrapper = styled.div`${tw`flex justify-center items-center h-screen`}`
