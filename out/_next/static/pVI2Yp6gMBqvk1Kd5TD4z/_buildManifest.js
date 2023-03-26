a6df238f3bf2e5e6de696f833535f6fd9a3cd1ed
Tony Dugué <duguetony@gmail.com>
2023-03-26 11:25:18 +0200

    add out folder -> fix memory problem on server & cannot compile

---
+++ out/_next/static/pVI2Yp6gMBqvk1Kd5TD4z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/94726e6d-d285faa0a8bf923f.js","static/chunks/161-79d7499fef52c3a4.js","static/chunks/pages/index-36053a7670dde48b.js"],"/_error":["static/chunks/pages/_error-29b3d7bb596dbd7b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
